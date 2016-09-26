c0d4045a94866b5b2e3c0f8c0f7d482a7d3e9756
Szymon Krajewski <szykra@gmail.com>
2016-09-26 23:21:41 +0200

    Use nunjucks templates

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var nunjucks = require('gulp-nunjucks');
 var browserSync = require('browser-sync').create();
 
 gulp.task('style', function () {
     return gulp.src('./scss/style.scss')
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./public/css'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ['style', 'build'], function () {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./public"
         }
     });
 
     gulp.watch('./scss/**/*.scss', ['style']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./build/**/*.html', ['build']);
+    gulp.watch("./public/*.html").on('change', browserSync.reload);
 });
+
+gulp.task('build', function () {
+    return gulp.src('./build/*.html')
+        .pipe(nunjucks.compile({}))
+        .pipe(gulp.dest('./public/'));
+})
